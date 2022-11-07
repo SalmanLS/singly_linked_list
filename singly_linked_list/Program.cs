85ffc61c2e8d44151d18eb97c745d30fe8eb7d51
SalmanLS <salmanluthfi456@gmail.com>
2022-11-07 17:02:24 +0700

    create public list and public void addnote

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -12,7 +12,36 @@ namespace single_linked_list
 
     class List
     {
+        Node START;
+        public List()
+        {
+            START = null;
 
+        }
+        public void addNote() // add a node in the list
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\n Enter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n Enter the roll name of the student: ");
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            if(START != null || (rollNo <= START.rollNumber))
+            {
+                if ((START != null)&&(rollNo == START.rollNumber))
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                newnode.next = START;
+                START = newnode;
+                return;
+            }
+
+
+        }
     }
 
 }

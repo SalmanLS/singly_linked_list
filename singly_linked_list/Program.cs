197f0959f5c7d8bd5fc5030bc65b59aea0d7f4bf
SalmanLS <salmanluthfi456@gmail.com>
2022-11-07 16:50:08 +0700

    declare variables in the node class

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -2,9 +2,12 @@
 
 namespace single_linked_list
 {
+    //each node consist of the information part adn link to the next node
     class Node
     {
-        
+        public int rollNumber;
+        public string name;
+        public Node next;
     }
 
     class List
